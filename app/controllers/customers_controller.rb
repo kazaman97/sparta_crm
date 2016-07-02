a7ad95b355af59f9effce743377755aa69a7330a
Kazama <narusai97@gmail.com>
2016-07-02 19:57:35 +0900

    完成

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,9 @@
 class CustomersController < ApplicationController
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
+   before_action :authenticate_user!
   def index
+    @q = Customer.search(params[:q])
+    @customers = @q.result.page(params[:page])
   end
 
   def new
@@ -7,17 +11,44 @@ class CustomersController < ApplicationController
   end
 
   def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      redirect_to @customer
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if  @customer.update(customer_params)
+      redirect_to @customer
+    else
+      render :edit
+    end
   end
 
   def show
   end
 
   def destroy
+    @customer.destroy
+    redirect_to customers_url
+  end
+
+private
+
+  def customer_params
+    params.require(:customer).permit(:family_name, :given_name, :email, :company_id, :post_id)
+  end
+
+  def set_customer
+    @customer = Customer
+  end
+
+  def set_customer
+    @customer = Customer.find(params[:id])
   end
 end
